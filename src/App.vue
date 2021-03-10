66984569dffe834684702be9d3c4018af06d205a
azusa5526 <azusa5526@gmail.com>
2021-03-10 15:04:20 +0800

    add click info

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,8 @@ export default {
             scaledSize: new this.google.maps.Size(70, 70), // scaled size
             origin: new this.google.maps.Point(0, 0), // origin
             anchor: new this.google.maps.Point(30, 30) // anchor
-          }
+          },
+          message: '強化系：增強自己或武器本來的性質。可將攻擊、防守和療傷等能力發揮到極致的系。'
         },
         {
           position: new this.google.maps.LatLng(24.801710797338675, 120.97214348517628),
@@ -59,7 +60,8 @@ export default {
             scaledSize: new this.google.maps.Size(70, 70), // scaled size
             origin: new this.google.maps.Point(0, 0), // origin
             anchor: new this.google.maps.Point(30, 30) // anchor
-          }
+          },
+          message: '放出系：氣在身體以外仍可以保持威力，可以發到遠離身體的地方。將身體的氣放出。'
         },
         {
           position: new this.google.maps.LatLng(24.800708890349018, 120.97214348517628),
@@ -68,7 +70,8 @@ export default {
             scaledSize: new this.google.maps.Size(70, 70), // scaled size
             origin: new this.google.maps.Point(0, 0), // origin
             anchor: new this.google.maps.Point(30, 30) // anchor
-          }
+          },
+          message: '操作系：可將自身的念注入物件從而控制之（如注入他人體內，可控制他人）。'
         },
         {
           position: new this.google.maps.LatLng(24.80023740190553, 120.97307838657736),
@@ -77,7 +80,8 @@ export default {
             scaledSize: new this.google.maps.Size(70, 70), // scaled size
             origin: new this.google.maps.Point(0, 0), // origin
             anchor: new this.google.maps.Point(30, 30) // anchor
-          }
+          },
+          message: '不屬於以上五系的念能力皆分為此類。'
         },
         {
           position: new this.google.maps.LatLng(24.800744251910007, 120.97402627272027),
@@ -86,16 +90,18 @@ export default {
             scaledSize: new this.google.maps.Size(70, 70), // scaled size
             origin: new this.google.maps.Point(0, 0), // origin
             anchor: new this.google.maps.Point(30, 30) // anchor
-          }
+          },
+          message: '具現化系：可將自己的氣的形態改造成自己想要的東西，即將氣實物化。'
         },
         {
           position: new this.google.maps.LatLng(24.80169901024468, 120.97401328797855),
           icon: {
-            url: require('@/assets/exhaust.png'), // url
+            url: require('@/assets/variety.png'), // url
             scaledSize: new this.google.maps.Size(70, 70), // scaled size
             origin: new this.google.maps.Point(0, 0), // origin
             anchor: new this.google.maps.Point(30, 30) // anchor
-          }
+          },
+          message: '變化系：可將產生的氣的形狀以及性質改變。'
         }
       );
     },
@@ -107,6 +113,18 @@ export default {
           position: this.features[i].position,
           icon: this.features[i].icon,
           map: this.map
+        });
+
+        const infowindow = new this.google.maps.InfoWindow({
+          content: `
+          <div id="content">
+            <p>${this.features[i].message}</p>
+          </div>
+        `,
+          maxWidth: 200
+        });
+        marker.addListener('click', () => {
+          infowindow.open(this.map, marker);
         });
       }
     },
