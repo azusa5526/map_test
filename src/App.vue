df45e3fb8b42689cec98a30945148850a806f617
azusa5526 <azusa5526@gmail.com>
2021-03-10 13:27:16 +0800

    套件版本

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <GmapMap id="map" ref="mapRef" :center="center" :options="mapOptions" :style="mapStyle">
+        <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center = m.position"
+        />
+        <gmap-polygon :paths="paths"></gmap-polygon>
+      </GmapMap>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+
+  data() {
+    return {
+      center: { lat: 24.799989869755034, lng: 120.97194222168014 },
+      paths: [
+        [
+          { lat: 24.79861519505178, lng: 120.98133084232963 },
+          { lat: 24.79861519505178, lng: 120.98233084232963 },
+          { lat: 24.79961519505178, lng: 120.98233084232963 },
+          { lat: 24.79961519505178, lng: 120.98133084232963 }
+        ],
+        [
+          { lat: 24.802182280179526, lng: 120.97310435606073 },
+          { lat: 24.801710797338675, lng: 120.97214348517628 },
+          { lat: 24.800708890349018, lng: 120.97214348517628 },
+          { lat: 24.80023740190553, lng: 120.97307838657736 },
+          { lat: 24.800744251910007, lng: 120.97402627272027 },
+          { lat: 24.80169901024468, lng: 120.97401328797855 }
+        ]
+      ]
+    };
+  },
+
+  computed: {
+    mapOptions() {
+      return {
+        zoom: 16,
+        mapTypeId: 'terrain'
+      };
+    },
+    mapStyle() {
+      return {
+        width: '100%',
+        height: 500 + 'px'
+      };
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import * as VueGoogleMaps from 'vue2-google-maps';
+// import GmapCustomMarker from 'vue2-gmap-custom-marker';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB-T-Ztg8-D0FElC2YiuAcuYIo3ec29ukE',
+    libraries: 'places,drawing' // This is required if you use the Autocomplete plugin
+  }
+});
+
+// Vue.component('gmap-custom-marker', GmapCustomMarker);
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount('#app');
