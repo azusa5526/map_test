b8302d66882eaf3b3a74b576f02aa50d67a31049
azusa5526 <azusa5526@gmail.com>
2021-03-10 14:39:41 +0800

    use google doc ver

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="app">
     <div class="container">
-      <GmapMap id="map" ref="mapRef" :center="center" :options="mapOptions" :style="mapStyle">
-        <GmapMarker
-          :key="index"
-          v-for="(m, index) in markers"
-          :position="m.position"
-          :clickable="true"
-          :draggable="true"
-          @click="center = m.position"
-        />
-        <gmap-polygon :paths="paths"></gmap-polygon>
-      </GmapMap>
+      <div class="google-map" id="map"></div>
     </div>
   </div>
 </template>
@@ -23,42 +13,129 @@ export default {
 
   data() {
     return {
+      google: null,
+      map: null,
+      features: [],
       center: { lat: 24.799989869755034, lng: 120.97194222168014 },
       paths: [
-        [
-          { lat: 24.79861519505178, lng: 120.98133084232963 },
-          { lat: 24.79861519505178, lng: 120.98233084232963 },
-          { lat: 24.79961519505178, lng: 120.98233084232963 },
-          { lat: 24.79961519505178, lng: 120.98133084232963 }
-        ],
-        [
-          { lat: 24.802182280179526, lng: 120.97310435606073 },
-          { lat: 24.801710797338675, lng: 120.97214348517628 },
-          { lat: 24.800708890349018, lng: 120.97214348517628 },
-          { lat: 24.80023740190553, lng: 120.97307838657736 },
-          { lat: 24.800744251910007, lng: 120.97402627272027 },
-          { lat: 24.80169901024468, lng: 120.97401328797855 }
-        ]
+        { lat: 24.802182280179526, lng: 120.97310435606073 },
+        { lat: 24.801710797338675, lng: 120.97214348517628 },
+        { lat: 24.800708890349018, lng: 120.97214348517628 },
+        { lat: 24.80023740190553, lng: 120.97307838657736 },
+        { lat: 24.800744251910007, lng: 120.97402627272027 },
+        { lat: 24.80169901024468, lng: 120.97401328797855 }
       ]
     };
   },
 
-  computed: {
-    mapOptions() {
-      return {
-        zoom: 16,
-        mapTypeId: 'terrain'
-      };
+  methods: {
+    initMap() {
+      this.google = window.google;
+      this.map = new this.google.maps.Map(document.getElementById('map'), {
+        center: this.center,
+        zoom: 17,
+        maxZoom: 20,
+        minZoom: 5,
+        streetViewControl: false,
+        mapTypeControl: false
+      });
     },
-    mapStyle() {
-      return {
-        width: '100%',
-        height: 500 + 'px'
-      };
+
+    addfeature() {
+      this.features.push(
+        {
+          position: new this.google.maps.LatLng(24.802182280179526, 120.97310435606073),
+          icon: {
+            url: require('@/assets/strengthen.png'), // url
+            scaledSize: new this.google.maps.Size(70, 70), // scaled size
+            origin: new this.google.maps.Point(0, 0), // origin
+            anchor: new this.google.maps.Point(30, 30) // anchor
+          }
+        },
+        {
+          position: new this.google.maps.LatLng(24.801710797338675, 120.97214348517628),
+          icon: {
+            url: require('@/assets/exhaust.png'), // url
+            scaledSize: new this.google.maps.Size(70, 70), // scaled size
+            origin: new this.google.maps.Point(0, 0), // origin
+            anchor: new this.google.maps.Point(30, 30) // anchor
+          }
+        },
+        {
+          position: new this.google.maps.LatLng(24.800708890349018, 120.97214348517628),
+          icon: {
+            url: require('@/assets/control.png'), // url
+            scaledSize: new this.google.maps.Size(70, 70), // scaled size
+            origin: new this.google.maps.Point(0, 0), // origin
+            anchor: new this.google.maps.Point(30, 30) // anchor
+          }
+        },
+        {
+          position: new this.google.maps.LatLng(24.80023740190553, 120.97307838657736),
+          icon: {
+            url: require('@/assets/especial.png'), // url
+            scaledSize: new this.google.maps.Size(70, 70), // scaled size
+            origin: new this.google.maps.Point(0, 0), // origin
+            anchor: new this.google.maps.Point(30, 30) // anchor
+          }
+        },
+        {
+          position: new this.google.maps.LatLng(24.800744251910007, 120.97402627272027),
+          icon: {
+            url: require('@/assets/realization.png'), // url
+            scaledSize: new this.google.maps.Size(70, 70), // scaled size
+            origin: new this.google.maps.Point(0, 0), // origin
+            anchor: new this.google.maps.Point(30, 30) // anchor
+          }
+        },
+        {
+          position: new this.google.maps.LatLng(24.80169901024468, 120.97401328797855),
+          icon: {
+            url: require('@/assets/exhaust.png'), // url
+            scaledSize: new this.google.maps.Size(70, 70), // scaled size
+            origin: new this.google.maps.Point(0, 0), // origin
+            anchor: new this.google.maps.Point(30, 30) // anchor
+          }
+        }
+      );
+    },
+
+    addMarkers() {
+      for (let i = 0; i < this.features.length; i++) {
+        // eslint-disable-next-line no-unused-vars
+        const marker = new this.google.maps.Marker({
+          position: this.features[i].position,
+          icon: this.features[i].icon,
+          map: this.map
+        });
+      }
+    },
+
+    addPolygon() {
+      const hex = new this.google.maps.Polygon({
+        paths: this.paths,
+        strokeColor: '#b3023a',
+        strokeOpacity: 0.6,
+        strokeWeight: 3,
+        fillColor: '#f7a4c0',
+        fillOpacity: 0.35
+      });
+      hex.setMap(this.map);
     }
+  },
+
+  mounted() {
+    this.initMap();
+    this.addfeature();
+    this.addMarkers();
+    this.addPolygon();
   }
 };
 </script>
 
 <style lang="scss">
+.google-map {
+  width: 100%;
+  height: 600px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-import * as VueGoogleMaps from 'vue2-google-maps';
-// import GmapCustomMarker from 'vue2-gmap-custom-marker';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyB-T-Ztg8-D0FElC2YiuAcuYIo3ec29ukE',
-    libraries: 'places,drawing' // This is required if you use the Autocomplete plugin
-  }
-});
-
-// Vue.component('gmap-custom-marker', GmapCustomMarker);
 
 new Vue({
   render: (h) => h(App)
